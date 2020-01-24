b9bdcadc21bbd5f5fb0536d1e421f9b1722e5128
vishishtpriyadarshi <vishishtpriyadarshi867@gmail.com>
2020-01-24 10:21:42 +0000

    Created Home Page

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import Header from '../Common/Header';
+import image from '../assets/img/header-bg.jpg';
+
+class Home extends Component{
+	render(){
+		return(
+			<div>
+				<Header 
+					title="Study Group at IIT Guwahati focused on learning Artificial Intelligence."
+					subtitle=""
+					buttonText="About Us"
+					link="/about"
+					showButton={true}
+					image={image}
+				/>
+			</div>
+		)	
+	
+	}
+}
+
+export default Home;
