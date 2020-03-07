03a590173ed7e60c8518d0a6bc06bfae86f78eef
vishishtpriyadarshi <vishishtpriyadarshi867@gmail.com>
2020-03-07 11:58:26 +0000

    Initiatives Page Updated

--- src/components/blocks/InitiativeBlock.js
+++ src/components/blocks/InitiativeBlock.js
@@ -12,46 +12,49 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import ButtonBase from '@material-ui/core/ButtonBase';
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 245
-  },
-  media: {
-    height: 140
-  }
-});
-
 const useStylesGrid = makeStyles(theme => ({
   root: {
     flexGrow: 1,
     margin: "20px 20px",
-    height: "75%",
-    width: "90%",
+    height: "90%",
+    width: "80%",
+	
     align: "center"
   },
   paper: {
-    padding: theme.spacing(),
-    textAlign: "center",
-    backgroundColor: "#7986cb",
+    padding: theme.spacing(1),
+	marginLeft: '140px',
+    width: 1200,
     color: theme.palette.primary.contrastText
+  },
+  media: {
+    height: 250,
+    width: '100%',
+	align: "center",
+	margin: 'auto'
   }
 }));
 
 export default function CenteredGrid() {
-  const classes = useStylesGrid();
+  	const classes = useStylesGrid();
 
   return (
     // <div className={classes.root}> onClick={() => alert("Hello from here")}>
     <div className={classes.root}>
-      <Grid container spacing={3}>
-        <Grid item xs={6} style={{ display: "flex" }}>
-          <Card className={classes.root}>
+		
+	<Paper className={classes.paper} elevation={3}>  
+		<h1 style={{textAlign: 'center', color: "#4169E1", fontWeight: '1500'}}> Initiatives</h1>
+      <Grid container spacing={5} justify="center" alignItems="center">
+        <Grid item xs={6} style={{ display: "flex"}}>
+			<Card className={classes.root}>
             <CardActionArea>
               <CardMedia
-                //className={classes.media}
+                className={classes.media}
                 component="img"
-                image="/img/project_images/interiit.jpeg"
+                image="/img/project_images/interiit3.png"
                 title="Inter IIT"
+				onClick={event => window.location.href='https://www.google.com'}
+				
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
@@ -64,24 +67,24 @@ export default function CenteredGrid() {
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
+				{/*<Button size="small" color="primary" onClick={event => window.location.href='https://www.google.com'}>
+                	Learn More
+              	</Button>*/}
               </CardActions>
           </Card>
           
         </Grid>
+		  
         <Grid item xs={6} style={{ display: "flex" }}>
           <Card className={classes.root}>
             <CardActionArea>
               <CardMedia
-                //className={classes.media}
+                className={classes.media}
                 component="img"
-                image="/img/project_images/hackathons.jpg"
+                image="/img/project_images/pdiscussions.jpeg"
                 title="Hackathons"
+				onClick={event => window.location.href='https://www.google.com'}
+				
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
@@ -94,12 +97,9 @@ export default function CenteredGrid() {
               </CardContent>
             </CardActionArea>
             <CardActions>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
+				{/*<Button size="small" color="primary" onClick={event => window.location.href='https://www.google.com'}>
+                	Learn More
+              	</Button>*/}
               </CardActions>
           </Card>
         </Grid>
@@ -107,10 +107,12 @@ export default function CenteredGrid() {
           <Card className={classes.root}>
             <CardActionArea>
               <CardMedia
-                //className={classes.media}
+                className={classes.media}
                 component="img"
-                image="/img/project_images/pdiscussions.jpeg"
+                image="/img/project_images/hackathons.jpg"
                 title="Paper Discussions"
+				onClick={event => window.location.href='https://www.google.com'}
+				  
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
@@ -122,16 +124,22 @@ export default function CenteredGrid() {
                 </Typography>
               </CardContent>
             </CardActionArea>
+			  {/* <CardActions>
+               	<Button size="small" color="primary" onClick={event => window.location.href='https://www.google.com'}>
+                	Learn More
+              	</Button>
+              </CardActions>*/}
           </Card>
         </Grid>
         <Grid item xs={6} style={{ display: "flex" }} padding={20}>
           <Card className={classes.root}>
             <CardActionArea>
               <CardMedia
-                //className={classes.media}
+                className={classes.media}
                 component="img"
-                image="/img/project_images/alumni.jpeg"
+                image="/img/project_images/sail.jpg"
                 title="Alumni Interactions"
+				onClick={event => window.location.href='https://www.google.com'}
               />
               <CardContent>
                 <Typography gutterBottom variant="h5" component="h2">
@@ -143,9 +151,18 @@ export default function CenteredGrid() {
                 </Typography>
               </CardContent>
             </CardActionArea>
+			  {/*<CardActions>
+				   
+				   <Button size="small" color="primary" onClick={event => window.location.href='https://www.google.com'}>
+                	Learn More
+              		</Button>
+				  
+              
+              </CardActions>*/}
           </Card>
         </Grid>
       </Grid>
+		</Paper>
     </div>
   );
 }
