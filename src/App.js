50be7239d27623bec5bafea349f71876dec267c7
vishishtpriyadarshi <vishishtpriyadarshi867@gmail.com>
2021-03-23 06:53:09 +0000

    Changes in the repo name to fix some deployment bugs

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MainPage from './components/mainPage';
 
 function App() {
   return (
-    <BrowserRouter basename={'/IITG.AI-Website/'}>
+    <BrowserRouter>
       <MainPage />
     </BrowserRouter>
   );
