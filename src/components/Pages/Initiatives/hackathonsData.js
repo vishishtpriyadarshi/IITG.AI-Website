6cb213240bc146a636ca6d6764d378408bcbb40c
vishishtpriyadarshi <vishishtpriyadarshi867@gmail.com>
2021-04-06 08:11:39 +0000

    Initiatives page updated

--- src/components/Pages/Initiatives/hackathonsData.js
+++ src/components/Pages/Initiatives/hackathonsData.js
@@ -4,7 +4,7 @@ export default [
         header: null,
         title: "Intra Hostel Challenge - AI in India",
         subtitle: null,
-        description: "As part of Kriti 2019, IITG.ai for the first time conducted a competition and asked participants to develop solutions to problems in India using AI. A total of 8 hostels participated and we saw diverse ideas from AI in mental health, in medical imaging and even in soil characteristic prediction! ",
+        description: "As part of Kriti 2019, IITG.ai for the first time conducted a competition and asked participants to develop solutions to problems in India using AI.\n\nA total of 8 hostels participated and we saw diverse ideas from AI in mental health, in medical imaging and even in soil characteristic prediction! ",
         col: "12",
         image: "KritiHackathon.jpg",
 		link: "https://github.com/IITGuwahati-AI/Kriti ",
@@ -13,7 +13,7 @@ export default [
         header: null,
         title: "Transfer Learning Hackathon",
         subtitle: null,
-        description: "The hackathon started with a lecture from our senior on transfer learning from a practical and implementation perspective. The problem statement was that we had to pick a problem/dataset of our choice and apply transfer learning to it. Narrowing down to a good dataset to work on took the majority of time. Teams came up with interesting problems such as Face2Text, Flower Classification, Sound Captioning and so on. Face2text takes images of faces as input and attempts to describe the face based on facial features and emotional expressions. All the teams presented their solutions where we were questioned and graded based on novelty, the application of transfer learning and the working of the model. Results they were subsequently announced and Face2text stood first.",
+        description: "The hackathon started with a lecture from our senior on transfer learning from a practical and implementation perspective. The problem statement was that we had to pick a problem/dataset of our choice and apply transfer learning to it. Narrowing down to a good dataset to work on took the majority of time. Teams came up with interesting problems such as Face2Text, Flower Classification, Sound Captioning and so on.\n\nFace2text takes images of faces as input and attempts to describe the face based on facial features and emotional expressions. All the teams presented their solutions where we were questioned and graded based on novelty, the application of transfer learning and the working of the model. Results they were subsequently announced and Face2text stood first.",
 		image: "TransferLearning.jpg",
 		link: "https://github.com/IITGuwahati-AI/Kriti ",
         col: "12"
@@ -22,7 +22,7 @@ export default [
         header: null,
         title: "Workshop on GANs",
         subtitle: "- Aditya Mehndiratta and Yashwanth M",
-        description: "The hackathon started with a lecture from our senior on transfer learning from a practical and implementation perspective. The problem statement was that we had to pick a problem/dataset of our choice and apply transfer learning to it. Narrowing down to a good dataset to work on took the majority of time. Teams came up with interesting problems such as Face2Text, Flower Classification, Sound Captioning and so on. Face2text takes images of faces as input and attempts to describe the face based on facial features and emotional expressions. All the teams presented their solutions where we were questioned and graded based on novelty, the application of transfer learning and the working of the model. Results they were subsequently announced and Face2text stood first.",
+        description: "The workshop consisted of the theoretical part of GANs as well as the hands-on code session. The discussion started with the introduction and maths behind Probabilistic Generative Models and Basic GANs introduced by Ian Goodfellow in 2014 and followed by a detailed discussion on DCGAN, Wasserstian GAN, BCE Loss, and Cycle GANs.\n\nProblems related to GANs like Mode Collapse, Vanishing Gradient,  Problems with Counting, Problems with Perspective, Problems with Global Structure were also discussed.\n\nIn hands-on code sessions, MNIST Handwritten images were created after training both Classical GAN and DCGAN. It also involved a code demo for Style GAN. Code in PyTorch, as well as Tensorflow, was discussed for all the topics.\n\nAssignment task was given to the participants. The task was to modify code or start from scratch and implement WGAN with gradient penalty and to explore one other method used to stabilize training and implement it.Applications of GANs such as Pix2Pix, Visually-Aware Fashion Recommendation, and Design, Photos to paintings, Star GAN, Style GAN were also discussed.",
 		image: "GAN_3.png",
 		link: "https://github.com/IITGuwahati-AI/IITG.ai_GAN",
         col: "12"
